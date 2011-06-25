fbace3d0bf4fb8d8560686b4b68627c1ff392f60
daniel.fsalves <daniel.fsalves@ec8a1f05-041f-0df4-a73d-0f109fbb4180>
2011-06-25 18:50:31 +0000

    GUI básica implementada em separado do modo em texto.

---
+++ src/simulator/GUIControl.java
@@ -0,0 +1,19 @@
+package simulator;
+
+/**
+ * Classe para testar em modo GUI o funcionamento do simulador.
+ * @author daniel
+ *
+ */
+public class GUIControl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SimulatorView sw = new SimulatorView();
+		sw.start();
+
+	}
+
+}
