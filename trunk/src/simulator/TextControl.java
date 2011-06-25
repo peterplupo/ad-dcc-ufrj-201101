7ce38f2296a49c25ad4d3c14e49ac7b8a2192304
daniel.fsalves <daniel.fsalves@ec8a1f05-041f-0df4-a73d-0f109fbb4180>
2011-06-25 18:50:31 +0000

    GUI básica implementada em separado do modo em texto.

--- trunk/src/simulator/TextControl.java
+++ trunk/src/simulator/TextControl.java
@@ -1,19 +1,41 @@
 package simulator;
 
+/**
+ * Classe para testar em console o funcionamento do Simulador
+ * @author daniel
+ *
+ */
 public class TextControl {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		SimulatorView sw = new SimulatorView();
-		sw.start();
+		testRun(0.2);
+		testRun(0.4);
+		testRun(0.6);
+		testRun(0.8);
 	}
 	
 	private static void testRun(double ro) {
-
+		SimulationManager simulationManager = new SimulationManager(0);
+		MetricsAgregator metricsAgregator;
 		
+		System.out.println("Iniciando teste com ro = " + ro + ".");
+		simulationManager.runSimulation(ro);
+		metricsAgregator = simulationManager.getMetricsAgregator();
+		
+		System.out.println("Resultados:");
+		System.out.println("E[T1] = " + metricsAgregator.getMeanTotal1() + " +- " + metricsAgregator.getDeviationTotal1());
+		System.out.println("E[W1] = " + metricsAgregator.getMeanAtraso1() + " +- " + metricsAgregator.getDeviationAtraso1());
+		System.out.println("E[N1] = " + metricsAgregator.getMeanNFila1() + " +- " + metricsAgregator.getDeviationNTotal1());
+		System.out.println("E[Nq1] = " + metricsAgregator.getMeanNAtraso1() + " +- " + metricsAgregator.getDeviationNAtraso1());
+		System.out.println("E[T2] = " + metricsAgregator.getMeanTotal2() + " +- " + metricsAgregator.getDeviationTotal2());
+		System.out.println("E[W2] = " + metricsAgregator.getMeanAtraso2() + " +- " + metricsAgregator.getDeviationAtraso2());
+		System.out.println("E[N2] = " + metricsAgregator.getMeanNFila2() + " +- " + metricsAgregator.getDeviationNTotal2());
+		System.out.println("E[Nq2] = " + metricsAgregator.getMeanNAtraso2() + " +- " + metricsAgregator.getDeviationNAtraso2());
+		System.out.println("V(W1) = " + metricsAgregator.getVarianceAtraso1());
+		System.out.println("V(W2) = " + metricsAgregator.getVarianceAtraso2());
 		
 	}
 
