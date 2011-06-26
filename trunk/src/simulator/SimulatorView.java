7406abdaa2b492606c7f49ee712e1dff6ee144f2
daniel.fsalves <daniel.fsalves@ec8a1f05-041f-0df4-a73d-0f109fbb4180>
2011-06-26 01:24:55 +0000

    Removendo warnings.

--- trunk/src/simulator/SimulatorView.java
+++ trunk/src/simulator/SimulatorView.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
