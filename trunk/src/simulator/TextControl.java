77bd48dbf456a21aad03a9eb60708f31a019fda2
daniel.fsalves <daniel.fsalves@ec8a1f05-041f-0df4-a73d-0f109fbb4180>
2011-06-29 22:20:52 +0000

    Valores calibrados.

--- trunk/src/simulator/TextControl.java
+++ trunk/src/simulator/TextControl.java
@@ -13,22 +13,30 @@ public class TextControl {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		testRun(0.2);
-		testRun(0.4);
-		testRun(0.6);
-		testRun(0.8);
+		testRun(10000, 100, 50, 0.2);
+		testRun(10000, 100, 100, 0.4);
+		testRun(15000, 500, 100, 0.6);
+		testRun(40000, 500, 100, 0.8);
+		testRun(200000, 1000, 100, 0.9);
 	}
 	
-	private static void testRun(double ro) {
+	private static void testRun(int eventsTransiente, int minEvents, int minFases, double ro) {
 		Random seed = new Random();
 		SimulationManager simulationManager = new SimulationManager(seed.nextInt());
 		MetricsAgregator metricsAgregator;
 		
+		simulationManager.setEventsTransient(eventsTransiente);
+		simulationManager.setMinEvents(minEvents);
+		simulationManager.setMinFases(minFases);
+		
 		System.out.println("Iniciando teste com ro = " + ro + ".");
+		System.out.println("Numero de fregueses na fase transiente: " + eventsTransiente);
+		System.out.println("Numero de fregueses por rodada: " + minEvents);
+		System.out.println("Numero de rodadas: " +  minFases);
 		simulationManager.runSimulation(ro);
 		metricsAgregator = simulationManager.getMetricsAgregator();
 		
-		System.out.println("Total de eventos processados: " + simulationManager.getTotalRodadas());
+		System.out.println("Total de fregueses processados: " + simulationManager.getTotalRodadas());
 		System.out.println("Resultados:");
 		System.out.println("E[T1] = " + metricsAgregator.getMeanTotal1() + " +- " + metricsAgregator.getDeviationTotal1());
 		System.out.println("E[W1] = " + metricsAgregator.getMeanAtraso1() + " +- " + metricsAgregator.getDeviationAtraso1());
