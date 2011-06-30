f127cfeef0f7b76c675504531ce328b16ef2f72c
daniel.fsalves <daniel.fsalves@ec8a1f05-041f-0df4-a73d-0f109fbb4180>
2011-06-30 03:10:34 +0000

    Simulador parece pronto, há novos testes e jar.

--- src/simulator/EventQueue.java
+++ src/simulator/EventQueue.java
@@ -32,6 +32,110 @@ public class EventQueue {
 	}
 	
 	/**
+	 * Metodo para testar a logica da fila
+	 * @return deve ser igual a 
+	 */
+	public String testProcessing() {
+		interval = stop = tempo = 0;
+		color = 0;
+		chegada1 = new EventClassA(0, color);
+		EventClassB chegada2 = queue.peekFirst();
+		String progressao = Double.toString(tempo);
+		
+		double tempoServico = 1;
+		double tempoDisponivel;
+		double proximaChegada = 0;
+		
+		while (!queue.isEmpty()) {
+			queue.pop();
+			
+		}
+		for (int i = 1; i <= 10; ++i) {
+			switch (i) {
+			case 1:
+				tempoServico = 1;
+				proximaChegada = 2;
+				break;
+			case 2:
+				tempoServico = 1;
+				break;
+			case 3:
+				tempoServico = 2;
+				proximaChegada = 3;
+				break;
+			case 4:
+				tempoServico = 2;
+				proximaChegada = 8;
+				break;
+			case 5:
+				tempoServico = 1;
+				break;
+			case 6:
+				tempoServico = 2;
+				break;
+			case 7:
+				tempoServico = 1;
+				proximaChegada = 20;
+				break;
+			case 8:
+				tempoServico = 2;
+				break;
+			case 9:
+				tempoServico = 1;
+				break;
+			case 10:
+				tempoServico = 1;
+				break;
+			}
+			chegada2 = queue.peekFirst();
+			if (queue.isEmpty() || chegada1.getTime() <= chegada2.getTime() || tempo >= chegada1.getTime()) {
+				//trata chegada1
+				System.out.println("1: " + chegada1.getTime() + "|" + tempo + "|" + tempoServico);
+				if (tempo < chegada1.getTime()) {
+					tempo = chegada1.getTime();
+				}
+				chegada1.servir(tempo, tempoServico);
+				tempo += tempoServico;
+				if (chegada1.getColor() == color) {
+					metricsCollection.collect(chegada1, getTimeInterval());
+				}
+				chegada1 = new EventClassA(proximaChegada, color);
+				queue.add(new EventClassB(tempo, color));
+			} else {
+				//trata chegada2
+				tempoDisponivel = chegada1.getTime() - tempo;
+				System.out.println("2: " + chegada2.getTime() + "|" + tempo + "|" + tempoServico + "|" + tempoDisponivel + "|" + chegada2.getTempoRestante());
+				
+				if (tempo < chegada2.getTime()) {
+					tempo = chegada2.getTime();
+				}
+				
+				if (chegada2.hasServico()) {
+					tempoServico = chegada2.getTempoRestante();
+					chegada2.continuarServico(tempo, tempoDisponivel);
+				} else {
+					chegada2.iniciarServico(tempo, tempoServico, tempoDisponivel);
+				}
+				
+				if (chegada2.hasServico()) {
+					tempo += tempoDisponivel;
+				} else {
+					tempo += tempoServico;
+					if (chegada2.getColor() == color) {
+						metricsCollection.collect(chegada2, getTimeInterval());
+					}
+					queue.pop();
+				}
+			}
+			progressao += "-" + tempo;
+		}
+		
+		System.out.println(progressao);
+		
+		return progressao;
+	}
+	
+	/**
 	 * Metodo para processar o proximo evento,
 	 * realizando as operações necessárias na fila
 	 * e capturando as métricas
@@ -58,12 +162,13 @@ public class EventQueue {
 			processEventClassA();
 		} else {
 			//trata chegada2
-			//System.out.println("Tratando chegada 2");
-			//System.out.println("chegada1: " + chegada1.getTime() + "; chegada2: " + chegada2.getTime() + "; tempo: " + tempo);
 			processEventClassB(chegada1.getTime() - tempo);
 		}
 	}
 	
+	/**
+	 * Serve um fregues na primeira fila
+	 */
 	private void processEventClassA() {
 		double tempoServico = exponencialServico.getValue();
 		
@@ -79,6 +184,11 @@ public class EventQueue {
 		queue.add(new EventClassB(tempo, color));
 	}
 
+	/**
+	 * Serve um fregues da segunda fila enquanto houver tempo disponivel, marcando-o como
+	 * em servico se for interrompido por uma chegada na outra fila
+	 * @param tempoDisponivel
+	 */
 	private void processEventClassB(double tempoDisponivel) {
 		EventClassB event = queue.peekFirst();
 		double tempoServico;
@@ -108,6 +218,9 @@ public class EventQueue {
 		}
 	}
 	
+	/**
+	 * Avanca para a proxima rodada
+	 */
 	public void advanceColor() {
 		metricsCollection.clean();
 		++color;
